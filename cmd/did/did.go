040c336c4217f48732e1d411e2c13545a021ed7b
Till Rönitz <till.roenitz@smail.inf.h-brs.de>
2026-04-29 08:37:54 +0200

    did: Use new backend, implement until flag.
    Sometimes it is not possible to always track hours as they pass. To
    make ues of did more ergonomic, the user can now determine until when
    they did the activity they entered.
    
    This is done via the -until flag. Any valid RFC3339 timestamp may be
    used, provided it specifies a point in time after time of last entry and
    before the current moment.

--- cmd/did/did.go
+++ cmd/did/did.go
@@ -1,27 +1,67 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/tillnr/hours/didfile"
 )
 
-func main() {
-	if len(os.Args) <= 1 {
-		usage()
-	}
-
-	didfile, err := didfile.Open()	
-	if err != nil {
-		println(err.Error())
-		return
-	}
-
-	didfile.Append(strings.Join(os.Args[1:], " "))
+type timeValue struct {
+	time.Time
 }
 
-func usage() {
-	println("usage: did ENTRY ...")
-	os.Exit(1)
+func (t timeValue) String() string {
+	return t.Time.Local().Format(time.RFC3339)
+}
+
+func (t timeValue) Set(maybeTime string) error {
+	parsed, err := parse(maybeTime)
+	t.Time = parsed
+	return err
+}
+
+func parse(maybeTime string) (time.Time, error) {
+	loc, err := time.LoadLocation("Local")
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	t, err := time.ParseInLocation(time.RFC3339, maybeTime, loc)
+	if err == nil {
+		return t, nil
+	}
+
+	return time.ParseInLocation(time.DateOnly, maybeTime, loc)
+}
+
+var until = timeValue{}
+
+func main() {
+	flag.Var(until, "until", "Make an entry lasting until the time specified. Must be after time of last entry and before the present moment. Must be in DateOnly or RFC3339 format.")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	didfile, err := didfile.Open()
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+
+	entry := strings.Join(args, " ")
+	if !until.Time.IsZero() {
+		err = didfile.AppendUntil(entry, until.Time)
+	} else {
+		err = didfile.Append(entry)
+	}
+
+	if err != nil {
+		println(err.Error())
+	}
 }
