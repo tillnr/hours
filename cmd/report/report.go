ca632d712596d3dd9c5f6e04213de5f3884369c8
Till Rönitz <till.roenitz@smail.inf.h-brs.de>
2026-04-29 08:58:19 +0200

    report: Use new backend, add from flag.
    Reports are now aggregated by activity. Sometimes, we want to generate
    report from a certain time onwards.
    
    This is solved by the -from flag. Any valid RFC3339 or DateOnly
    timestamp may be used. Only durations after the given timestamp are
    aggregated in the report.

--- cmd/report/report.go
+++ cmd/report/report.go
@@ -1,13 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"time"
 
 	"github.com/tillnr/hours/didfile"
 	"github.com/tillnr/hours/report"
 )
 
+type timeValue struct {
+	time.Time
+}
+
+func (t timeValue) String() string {
+	return t.Time.Local().Format(time.RFC3339)
+}
+
+func (t timeValue) Set(maybeTime string) error {
+	parsed, err := parse(maybeTime)
+	t.Time = parsed
+	return err
+}
+
+func parse(maybeTime string) (time.Time, error) {
+	loc, err := time.LoadLocation("Local")
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	t, err := time.ParseInLocation(time.RFC3339, maybeTime, loc)
+	if err == nil {
+		return t, nil
+	}
+
+	return time.ParseInLocation(time.DateOnly, maybeTime, loc)
+}
+
 func main() {
+	var from = timeValue{}
+	flag.Var(from, "from", "generate a report from point in time onwards")
+	flag.Parse()
+
 	didfile, err := didfile.Open()
 	if err != nil {
 		println(err.Error())
@@ -15,7 +49,7 @@ func main() {
 	}
 	defer didfile.Close()
 
-	report, err := report.Full(didfile.Reader())
+	report, err := report.From(from.Time, didfile.Reader())
 	if err != nil {
 		println(err.Error())
 		return
